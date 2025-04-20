9332b4a61d4e679e0740249fe00ab426d788d568
Athos Schrapett <athosschrapett@outlook.com>
2025-04-20 19:41:19 -0300

    fix: ajuste validações para contemplar valores de idade possiveis e limite maximo de idade.

--- back/ControlePessoas.Application/Validators/Pessoa/PessoaCreateDTOValidator.cs
+++ back/ControlePessoas.Application/Validators/Pessoa/PessoaCreateDTOValidator.cs
@@ -8,19 +8,27 @@ public class PessoaCreateDTOValidator : AbstractValidator<PessoaCreateDTO>
     {
         RuleFor(p => p.Nome)
             .NotEmpty()
-            .WithMessage("O nome é obrigatório.")
+                .WithMessage("O nome é obrigatório.")
             .MaximumLength(60);
 
         RuleFor(p => p.Idade)
             .GreaterThan(0)
-            .WithMessage("A idade deve ser maior que zero.");
+                .WithMessage("A idade deve ser maior que zero.")
+            .LessThanOrEqualTo(130)
+                .WithMessage("A idade deve ser no máximo 130 anos.");
 
         RuleFor(p => p.Sexo)
             .Must(s => s == 'M' || s == 'F')
-            .WithMessage("Sexo deve ser 'M' ou 'F'.");
+                .WithMessage("Sexo deve ser 'M' ou 'F'.");
 
         RuleFor(p => p.Peso)
             .GreaterThan(0)
-            .WithMessage("O peso deve ser maior que zero.");
+                .WithMessage("O peso deve ser maior que zero.");
+
+        RuleFor(p => p.Altura)
+            .LessThanOrEqualTo(2.30)
+                .WithMessage("A altura deve ser no máximo 2.30 metros.")
+            .GreaterThan(0.30)
+                .WithMessage("A altura deve ser maior que 0.30 metros.");
     }
 }
