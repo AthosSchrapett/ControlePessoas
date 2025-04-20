7b5bccbb9d73eaccf17545b8f23d80a8bccb6866
Athos Schrapett <athosschrapett@outlook.com>
2025-04-20 12:48:22 -0300

    feat: implementa entidade Pessoa com verificação de idoso e herança de BaseEntity

---
+++ back/ControlePessoas.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace ControlePessoas.Domain.Entities.Base;
+public abstract class BaseEntity
+{
+    public Guid Id { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime UpdatedAt { get; private set; }
+    public bool IsDeleted { get; private set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+
+    public void Atualizar()
+    {
+        UpdatedAt = DateTime.UtcNow;
+    }
+}
---
+++ back/ControlePessoas.Domain/Entities/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using ControlePessoas.Domain.Entities.Base;
+
+namespace ControlePessoas.Domain.Entities;
+public class Pessoa : BaseEntity
+{
+    public string Nome { get; private set; }
+    public int Idade { get; private set; }
+    public char Sexo { get; private set; }
+    public double Peso { get; private set; }
+    public double? Altura { get; private set; }
+    public bool Idoso { get; private set; }
+
+    public Pessoa
+    (
+        string nome, 
+        int idade, 
+        char sexo, 
+        double peso, 
+        double? altura
+    )
+    {
+        Nome = nome;
+        Idade = idade;
+        Sexo = sexo;
+        Peso = peso;
+        Altura = altura;
+        Idoso = VerificarIdoso();
+    }
+
+    private bool VerificarIdoso()
+    {
+        return Idade >= 60;
+    }
+}
