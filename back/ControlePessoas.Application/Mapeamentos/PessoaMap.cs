d57c80b884f7abb5acabde2164ea279688cec13e
Athos Schrapett <athosschrapett@outlook.com>
2025-04-20 15:59:19 -0300

    feat: adiciona classe de mapeamento entre Pessoa e DTOs

---
+++ back/ControlePessoas.Application/Mapeamentos/PessoaMap.cs
@@ -0,0 +1,60 @@
+﻿using ControlePessoas.Domain.DTOs.Create;
+using ControlePessoas.Domain.DTOs.Get;
+using ControlePessoas.Domain.DTOs.Update;
+using ControlePessoas.Domain.Entities;
+
+namespace ControlePessoas.Application.Mapeamentos;
+public static class PessoaMap
+{
+    public static Pessoa DTOParaPessoa(this PessoaCreateDTO dto)
+    {
+        return new Pessoa
+        (
+            dto.Nome,
+            dto.Idade,
+            dto.Sexo,
+            dto.Peso,
+            dto.Altura
+        );
+    }
+
+    public static void AtualizarPessoaPorDTO(this Pessoa pessoa, PessoaUpdateDTO dto)
+    {
+        pessoa.AtualizarDados
+        (
+            dto.Nome,
+            dto.Idade,
+            dto.Sexo,
+            dto.Peso,
+            dto.Altura
+        );
+    }
+
+    public static PessoaGetDTO PessoaParaDTO(this Pessoa pessoa)
+    {
+        return new PessoaGetDTO
+        (
+            pessoa.Id,
+            pessoa.Nome,
+            pessoa.Idade,
+            pessoa.Sexo,
+            pessoa.Peso,
+            pessoa.Altura,
+            pessoa.Idoso
+        );
+    }
+
+    public static IEnumerable<PessoaGetAllDTO> PessoasParaDTO(this IEnumerable<Pessoa> pessoas)
+    {
+        return pessoas.Select
+        (p => 
+            new PessoaGetAllDTO
+            (
+                p.Id,
+                p.Nome,
+                p.Idade,
+                p.Idoso
+            )
+        );
+    }
+}
